c36b62ee23e9f72903ad0b9f7bb6aa61f1773831
Prajwal <prajwaljadhav248@gmail.com>
2021-04-26 22:18:48 +0530

    v2 branch created

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello World"})
+    return jsonify({"message": "Hello World! This is v2"})
 
 
 @app.route('/index')
