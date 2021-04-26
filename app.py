6735943f43c6a400778abfcd9a23755736bfbc2c
Prajwal <prajwaljadhav248@gmail.com>
2021-04-26 23:37:41 +0530

    exta text added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello World! This is v2"})
+    return jsonify({"message": "Hello World! This is v2 with some extra text"})
 
 
 @app.route('/index')
@@ -15,3 +15,6 @@ def index():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5050, debug=True)
+
+
+
